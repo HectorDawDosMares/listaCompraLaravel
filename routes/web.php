4d54ff74a2a19377710470b2ac16381c561db34e
HectorDawDosMares <7946424@alu.murciaeduca.es>
2019-11-13 13:32:04 +0100

    Definicion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,36 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return "Pantalla principal";
+});
+
+Route::get('/login', function () {
+    return "Login usuario";
+});
+
+Route::get('/logout', function () {
+    return "Logout usuario";
+});
+
+Route::get('/productos', function () {
+    return "Listado productos";
+});
+
+Route::get('/productos/show/{id}', function ($id) {
+    return "Vista detalle producto " . $id;
+});
+
+Route::get('/productos/create', function () {
+    return "Añadir producto";
+});
+
+Route::get('/productos/edit/{id}', function ($id) {
+    return "Modificar producto " . $id;
 });
